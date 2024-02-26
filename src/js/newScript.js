b7ad1013ed701e71ba1a7a48b890250134102670
supersonic29 <supersonic29@gmail.com>
2024-02-25 20:13:46 -0500

    Update newScript.js

--- src/js/newScript.js
+++ src/js/newScript.js
@@ -199,7 +199,7 @@ const createMoveInputs = (move) => {
     return htmlString
 }
 
-const hardCodedChars = ['jun']
+const hardCodedChars = ['jun','reina']
 
 const main = async () => {
     for (let i = 0; i < hardCodedChars.length; i++) {
